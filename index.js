e23921e777fa75171ea8b22df25a17dcc7ede788
borovvski <brandonborovvski@gmail.com>
2022-08-16 12:11:43 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,115 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+let passwordOneEl = document.querySelector(".password-one");
+let passwordTwoEl = document.querySelector(".password-two");
+let length = 15;
+
+function generateRandomSign() {
+  let randomSign = Math.floor(Math.random() * 91) + 1;
+  return characters[randomSign];
+}
+
+function generateRandomPassword() {
+  let randomPassword = "";
+  for (let i = 0; i < length; i++) {
+    randomPassword += generateRandomSign();
+  }
+  return randomPassword;
+}
+const passwordOne = generateRandomPassword();
+const passwordTwo = generateRandomPassword();
+function generatePassword() {
+  passwordOneEl.textContent += passwordOne;
+  passwordTwoEl.textContent += passwordTwo;
+}
